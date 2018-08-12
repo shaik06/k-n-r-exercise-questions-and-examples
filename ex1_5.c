7438d61740097279ae2003c58e11fc0ddb57c7e0
shaik nisar ahmed <42187590+shaik06@users.noreply.github.com>
2018-08-12 23:07:29 +0530

    Create ex1_5.c

---
+++ ex1_5.c
@@ -0,0 +1,9 @@
+//Modify the temperature conversion program to print the table in reverse order, that is, from 300 degrees to 0.
+
+#include<stdio.h>
+int main()
+{
+  float fahr;
+  for(fahr=0;fahr>=0;fahr--)
+      printf("%6.2f\u2103\t=\t\u2109\n",fahr,((5.0/9.0)*(fahr-32.0));
+ }
