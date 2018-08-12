628a8d0bca0d55095394bbbc1ac33f9812d54bfc
shaik nisar ahmed <42187590+shaik06@users.noreply.github.com>
2018-08-12 23:57:10 +0530

    Create ex1_7.c

---
+++ ex1_7.c
@@ -0,0 +1,13 @@
+Write a program to print the value of EOF.
+
+#include<stdio.h>
+int main()
+{
+  printf("%d\n",EOF);
+}
+
+/* code feedback:
+  
+  -> 'EOF' is a symbolic constant defined in 'stdio.h' header file.
+   
+*/
